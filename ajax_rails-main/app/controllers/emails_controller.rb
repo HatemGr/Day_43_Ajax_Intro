0971f6ff91fe009227373a1906ccc05cc72fd20a
HatemGr <hatemgribi@gmail.com>
2022-06-01 20:25:06 +0200

    Pofinage

--- ajax_rails-main/app/controllers/emails_controller.rb
+++ ajax_rails-main/app/controllers/emails_controller.rb
@@ -23,20 +23,12 @@ class EmailsController < ApplicationController
   def update 
     @email = Email.find(params[:id])
     @job = params[:job]
-    puts "#" * 50
-    puts params
-    puts "#" * 50
+
 
     if @job == "to_not_read"
       @email.update(read: false)
-      puts "@" * 50
-      puts "email read to #{@email.read}"
-      puts "@" * 50
     else
       @email.update(read: true)
-      puts "@" * 50
-      puts "email read to #{@email.read}"
-      puts "@" * 50
     end
 
     respond_to do |format|
