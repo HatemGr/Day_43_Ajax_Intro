c166ca38d076a53b1de0bf73d3a3ee0faa931dce
HatemGr <hatemgribi@gmail.com>
2022-06-01 19:44:52 +0200

    read unread done

--- ajax_rails-main/app/controllers/emails_controller.rb
+++ ajax_rails-main/app/controllers/emails_controller.rb
@@ -5,6 +5,7 @@ class EmailsController < ApplicationController
 
   def show 
     @email = Email.find(params[:id])
+    @email.update(read: true)
     respond_to do |format|
       format.html { redirect_to root_path }
       format.js { }
@@ -13,6 +14,31 @@ class EmailsController < ApplicationController
 
   def create 
     @email = Email.create(object: Faker::Game.title,body: Faker::Lorem.paragraph)
+    respond_to do |format|
+      format.html { redirect_to root_path }
+      format.js { }
+    end
+  end
+
+  def update 
+    @email = Email.find(params[:id])
+    @job = params[:job]
+    puts "#" * 50
+    puts params
+    puts "#" * 50
+
+    if @job == "to_not_read"
+      @email.update(read: false)
+      puts "@" * 50
+      puts "email read to #{@email.read}"
+      puts "@" * 50
+    else
+      @email.update(read: true)
+      puts "@" * 50
+      puts "email read to #{@email.read}"
+      puts "@" * 50
+    end
+
     respond_to do |format|
       format.html { redirect_to root_path }
       format.js { }
--- ajax_rails-main/app/helpers/emails_helper.rb
+++ ajax_rails-main/app/helpers/emails_helper.rb
@@ -1,5 +1,5 @@
 module EmailsHelper
   def email_class(email)
-    email.read ? 'link-dark text-decoration-none fw-light' : 'link-dark text-decoration-none table-dark fw-bold'
+    email.read ? 'link-dark text-decoration-none fw-light' : 'link-dark text-decoration-none table-warning fw-bold'
   end
 end
